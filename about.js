72de1f12dca869b0360c7cb2c02a0f8c8da1f310
akin_codes <adebayoakinade11@gmail.com>
2026-02-03 00:27:14 +0100

    Add files via upload

---
+++ about.js
@@ -0,0 +1,9 @@
+function aboutShowNav() {
+  const aboutSidebar = document.querySelector('.aboutSidebar')
+  aboutSidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+  const aboutSidebar = document.querySelector('.aboutSidebar')
+  aboutSidebar.style.display = 'none';
+}
